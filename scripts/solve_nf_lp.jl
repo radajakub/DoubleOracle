5a5df930356fb6cce243c99ef05fad6fdd3604f1
Jakub Rada <dev.jakubrada@icloud.com>
2023-01-31 23:33:02 +0100

    Add generation parameters to scripts

--- scripts/solve_nf_lp.jl
+++ scripts/solve_nf_lp.jl
@@ -9,7 +9,17 @@ if length(ARGS) >= 1
     nfg = load(ARGS[1], NormalFormGame)
 else
     println("Generating random game")
-    nfg = generate(NormalFormGame)
+
+    # generation parameters
+    A1min = 2
+    A1max = 5
+    A2min = 2
+    A2max = 5
+    minutil = -10
+    maxutil = 10
+    utilstep = 1
+
+    nfg = generate(NormalFormGame; A1min, A1max, A2min, A2max, minutil, maxutil, utilstep)
 end
 
 print(nfg)
